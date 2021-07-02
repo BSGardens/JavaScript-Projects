ccf341f381299c36d7f402352c5cfa38cf5e7e64
BSGardens <ben.s.becker@gmail.com>
2021-07-02 12:11:40 -0500

    basic js challenges

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,43 @@
+window.alert("I've got a bad feeling about this!") //window alert
+
+var A = "I've still got a bad feeling about this!<br>"
+document.write(A)
+
+var B = "This is going to be rough!<br>"
+document.write(B)
+
+var C = "What is next?"
+
+window.alert(C)
+
+document.write("Pennywise exclaimed, \"Y'all float down here.\"<br>\"I"
++ " don't think that is how the quote goes!\" Georgie said.<br>");
+
+document.write("\Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss<br>");
+
+document.write(100+100); //expression
+
+var TCM = "<br>Leatherface<br>", Scream = "<br>Ghostface<br>", CP = "<br>Chucky<br>", //multiple var assign
+    F13 = "<br>Jason<br>", Halloween = "<br>Michael<br>";
+
+var D = "<br>I wish it was" + " Halloween<br>" //concatenated string
+var D = D.fontcolor("orange");
+document.write(D)
+
+var TCM = TCM.fontcolor("red");  //assigning font color to var
+var Scream = Scream.fontcolor("red");
+var CP = CP.fontcolor("red");
+var F13 = F13.fontcolor("red");
+var Halloween = Halloween.fontcolor("red");
+document.write(TCM, Scream, CP, F13, Halloween)
+
+
+
+
+
+
+
+    
+
